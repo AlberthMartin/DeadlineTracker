581a0cf4609ac65f3e9efdac1144a5e6b657df63
AlberthMartin <130827415+AlberthMartin@users.noreply.github.com>
2025-09-04 17:56:39 +0300

    Project initialized

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+
+
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Example route
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+  .catch((err) => console.log(err));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+import { Button, HStack } from "@chakra-ui/react"
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <HStack>
+        <Button>Click me</Button>
+        <Button>Click me</Button>
+      </HStack>
+    </>
+  );
+}
+
+export default App;
